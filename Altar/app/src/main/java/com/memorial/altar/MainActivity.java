c414d8add8dcf2ee68a09ce02ac44a3e47393ca0
ysera34 <ycyoon34@gmail.com>
2017-08-26 15:33:13 +0900

    navigation drawer

--- Altar/app/src/main/java/com/memorial/altar/MainActivity.java
+++ Altar/app/src/main/java/com/memorial/altar/MainActivity.java
@@ -1,13 +1,20 @@
 package com.memorial.altar;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar mToolbar;
     private TextView mToolbarStarTextView;
@@ -26,6 +33,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mToolbarStarTextView = mToolbar.findViewById(R.id.toolbar_star_text_view);
         mToolbarStarTextView.setOnClickListener(this);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -36,5 +52,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mToolbarStarTextView.setText(String.valueOf(20));
                 break;
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_notifications:
+                break;
+            case R.id.nav_stars:
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_faq:
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
